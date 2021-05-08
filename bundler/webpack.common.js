74f04c3fa7757482f38af3ec7069e661f92bfd98
Saheb Giri <iamsahebgiri>
2021-05-09 02:19:00 +0530

    📦 NEW: Add webpack config and esbuild

---
+++ bundler/webpack.common.js
@@ -0,0 +1,63 @@
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCSSExtractPlugin = require("mini-css-extract-plugin");
+const path = require("path");
+
+module.exports = {
+  entry: path.resolve(__dirname, "../js/script.js"),
+  output: {
+    filename: "bundle.[contenthash].js",
+    path: path.resolve(__dirname, "../dist"),
+  },
+  devtool: "source-map",
+  plugins: [
+    new CopyWebpackPlugin({
+      patterns: [{ from: path.resolve(__dirname, "../assets") }],
+    }),
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "../index.html"),
+      minify: true,
+    }),
+    new MiniCSSExtractPlugin(),
+  ],
+  module: {
+    rules: [
+      // HTML
+      {
+        test: /\.(html)$/,
+        use: ["html-loader"],
+      },
+
+      // JS
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ["esbuild-loader"],
+      },
+
+      // CSS
+      {
+        test: /\.css$/,
+        use: [MiniCSSExtractPlugin.loader, "css-loader"],
+      },
+
+      {
+        test: /\.(jpg|png|gif|svg)$/,
+        type: "asset/resource",
+      },
+
+      // Fonts
+      {
+        test: /\.(ttf|eot|woff|woff2)$/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              outputPath: "assets/fonts/",
+            },
+          },
+        ],
+      },
+    ],
+  },
+};
